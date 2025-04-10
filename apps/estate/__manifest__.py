6ff1304a16af9ad190054dbab92fc3ff113f0cdb
Marco Santos <marco.inctva2@gmail.com>
2025-04-10 00:40:37 -0500

    chapter 3 of server framework 101

---
+++ apps/estate/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': "Marco's estate app 2",
+    'version': '1.0',
+    'author': 'Marco',
+    'depends': ['base'],
+    'application': True,
+    'category': 'Real Estate',
+    'description': 'Real estate management module',
+    'data': [],
+}
