597c8abafd9d802c5bdf4e594e01786cf05d5adb
Marco Santos <marco.inctva2@gmail.com>
2025-04-25 00:37:38 -0500

    added property_ids, statusbar widget, list order in models, model ordering and manual ordering

--- apps/estate/models/estate_property_type.py
+++ apps/estate/models/estate_property_type.py
@@ -4,9 +4,12 @@ from dateutil.relativedelta import relativedelta
 class EstatePropertyType(models.Model):
     _name = 'estate.property.type'
     _description = 'Real Estate Property Type'
+    _order = 'name'
 
     name = fields.Char(required=True)
     description = fields.Text()
+    property_ids = fields.One2many('estate.property', 'property_type_id', string='Properties')
+    sequence = fields.Integer(default=1)
 
     _sql_constraints = [
         ('check_name', 'UNIQUE(name)', 'Name must be unique'),
